8347b42e241dffc2b27c762f784b8687ad36a812
walker.benw <walker.benw@3e0122f0-23d6-11df-9f67-6fc5ad5a3b7e>
2010-02-28 04:50:16 +0000

    CHECK THIS SEXY SHIT

--- caribou/morse_window.py
+++ caribou/morse_window.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 
 import gtk
+import colorhandler
 
 class MorseWindow:
     
+    colorHandler = colorhandler.ColorHandler()
+
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.morseRoot = gtk.Button("")
@@ -20,8 +23,30 @@ class MorseWindow:
         self.fixed.show()
         self.window.show()
 
+    def colorKeys(self, startingNode):
+	self.colorHandler.colorAll(colorhandler.ColorOptions.standard) #Reset all colors to gray
+	self.colorHandler.setColorFromChar(startingNode.value, colorhandler.ColorOptions.morseCurrentNode) #color our current node
+	leftNode = startingNode.left
+	rightNode = startingNode.right
+	if leftNode != None:
+		self.colorHandler.setColorFromChar(leftNode.value, colorhandler.ColorOptions.morseLeftNode) #color our direct descendant
+		self.recursiveColorNodes(leftNode, colorhandler.ColorOptions.morseLeftNode) #and color its descendants
+	if rightNode != None:
+		self.colorHandler.setColorFromChar(rightNode.value, colorhandler.ColorOptions.morseRightNode) #color our direct descendant
+		self.recursiveColorNodes(rightNode, colorhandler.ColorOptions.morseRightNode) #and color its descendants
+
+    def recursiveColorNodes(self, startingNode, color):
+	if startingNode.left != None: #If we have an instantiated node to go to
+	    self.colorHandler.setColorFromChar(startingNode.left.value, color)
+	    self.recursiveColorNodes(startingNode.left, color)
+	if startingNode.right != None: #If we have an instantiated node to go to
+	    self.colorHandler.setColorFromChar(startingNode.right.value, color)
+	    self.recursiveColorNodes(startingNode.right, color)
+
+
     def refresh(self, currentNode):
         if currentNode != None:
+	    self.colorKeys(currentNode)
             self.morseRoot.set_label(currentNode.value)
             if currentNode.left != None:
                 self.morseLeft.set_label(currentNode.left.value)
