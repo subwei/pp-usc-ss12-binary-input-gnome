d62d7675af5bba4eee114d2bd39223d3f10f810b
walker.benw <walker.benw@3e0122f0-23d6-11df-9f67-6fc5ad5a3b7e>
2010-02-28 02:27:52 +0000

    Keyboard now get a lot of awesome colors as you click things.

---
+++ caribou/colorhandler.py
@@ -0,0 +1,37 @@
+import gtk
+
+class ColorOptions: #This is basically just an enum
+	standard = 0
+	morseLeftNode = 1
+	morseRightNode = 2
+	test = 3
+
+class ColorHandler:
+
+ 	e = gtk.Entry()
+	map = e.get_colormap()
+
+	buttonList = dict() #key = letter of the button, value = style of button (that style will use color colorOption)
+	morseLeftNodeColor = map.alloc_color("#7171f0")
+	morseRightNodeColor = map.alloc_color("#f07171")
+	standardColor = map.alloc_color("#cccccc")
+	testColor = map.alloc_color("#ff99ff")
+	colorMap = {ColorOptions.standard: standardColor, ColorOptions.morseLeftNode: morseLeftNodeColor, ColorOptions.morseRightNode: morseRightNodeColor, ColorOptions.test: testColor}  
+
+	def addButton(self, button, colorOption):
+		label = button.get_label()
+		self.buttonList[label] = button
+		self._doColoring(button, colorOption)
+
+	def setColor(self, encodedchar, colorOption):
+		char = unichr(encodedchar)
+		self._doColoring(self.buttonList.get(char), colorOption)
+
+	def _doColoring(self, button, colorOption):
+		button.modify_bg(gtk.STATE_NORMAL, self.colorMap.get(colorOption))
+		button.modify_bg(gtk.STATE_ACTIVE, self.colorMap.get(colorOption))
+
+	def _colorAll(self, colorOption):
+		for button in self.buttonList.values():
+			button.modify_bg(gtk.STATE_NORMAL, self.colorMap.get(colorOption))
+			button.modify_bg(gtk.STATE_ACTIVE, self.colorMap.get(colorOption))
